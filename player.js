151af373d335dab65e79d2801bf4a75245c4343b
kuzya81 <80736934+kuzya81@users.noreply.github.com>
2021-05-30 08:08:20 +0300

    Add files via upload

--- player.js
+++ player.js
@@ -3,13 +3,24 @@ let player;
         player = new YT.Player("player__content", {
           height: '390',
           width: '660',
-          videoId: 'M7lc1UVf-VE',
+          videoId: 'KH_crCj4Fjo',
           playerVars: {
             'playsinline': 1
           },
           events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            // 'onReady': onPlayerReady,
+            // 'onStateChange': onPlayerStateChange
+          },
+          playerVars:{
+            controls:0,
+            disablekb:0,
+            showinfo:0,
+            rel:0,
+            autoplay:0,
+            modestbranding:0,
+            widget_referrer:0,
           }
+        
+
         });
       }
