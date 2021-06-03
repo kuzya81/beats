def9e452032d68cf102942fe466945c501ae96e5
kuzya81 <80736934+kuzya81@users.noreply.github.com>
2021-06-03 07:45:45 +0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,10 +1,45 @@
-$('.slider__item').on('click', (e)=>{
-        
-    $('.slider__item').removeClass('slider__item-active');
+const mesureWidth = item =>{
+    const screenWidth = $(window).width();
+    const container = item.closest(".slider__list");
+    const titleBlock = container.find(".slider__item");
+    const titleWidth = titleBlock.width() * titleBlock.length;
 
-});
-$('.slider__item').on('click', function(e){
-        
-    $(this).addClass('slider__item-active');
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+    if(isMobile){
+
+        return screenWidth - titleWidth - 40;
+    } else{
+        return 500;
+    };
+
+}
+const closeEveryItemInContainer = container =>{
+    const items = container.find(".slider__item");
+    const content = container.find(".slider__item-content");
+
+    items.removeClass("slider__item-active");
+    content.width(0);
+}
+const openItem = (item) =>{
+    const hiddenContent = item.find(".slider__item-content");
+    const reqWidth = mesureWidth(item);
+    item.addClass("slider__item-active");
+    
+    hiddenContent.width(reqWidth);
+}
+
+$(".slider__item-title").on('click', e =>{
+ const $this = $(e.currentTarget);
+ const item =$this.closest(".slider__item");
+ const itemOpened = item.hasClass("slider__item-active");
+ const container = $this.closest(".slider__list");
+
+ if (itemOpened){
+     closeEveryItemInContainer(container);
+
+ } else {
+    closeEveryItemInContainer(container);
+     openItem(item);
+ };
 
 });
